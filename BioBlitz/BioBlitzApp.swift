525eb37c08c509bbda1e7a0e7d187db0d5b0bf1c
Gavin Marler <gavin.marler17@gmail.com>
2023-04-11 17:47:50 -0500

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Gavin Marler on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
