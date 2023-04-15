f73fb4a49279f7e31af04e9f99c4c923d00cd814
Gavin Marler <gavin.marler17@gmail.com>
2023-04-14 21:19:12 -0500

    first commit

---
+++ BioBlitz/BacteriaView.swift
@@ -0,0 +1,20 @@
+//
+//  BacteriaView.swift
+//  BioBlitz
+//
+//  Created by Gavin Marler on 4/11/23.
+//
+
+import SwiftUI
+
+struct BacteriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BacteriaView_Previews: PreviewProvider {
+    static var previews: some View {
+        BacteriaView()
+    }
+}
---
+++ BioBlitz/GameBoard.swift
@@ -0,0 +1,8 @@
+//
+//  GameBoard.swift
+//  BioBlitz
+//
+//  Created by Gavin Marler on 4/11/23.
+//
+
+import Foundation
